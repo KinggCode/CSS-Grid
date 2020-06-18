cba063937b7b3947756201b31e41eb6dcdd6404a
KinggCode <eugenio.parker3@gmail.com>
2020-06-18 05:07:51 +0000

    CSS-grid add

---
+++ css grid/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    
+    $("#sec1").click(function(){
+      $("#sec1 p").html("O");
+    })
+    $("#sec2").click(function(){
+        $("#sec2 p").html("X");
+      })
+    $("#sec3").click(function(){
+        $("#sec3 p").html("O");
+        })
+    $("#sec4").click(function(){
+        $("#sec4 p").html("X");
+        })
+    $("#sec5").click(function(){
+        $("#sec5 p").html("O");
+        })
+    $("#sec6").click(function(){
+        $("#sec6 p").html("X");
+        })
+    $("#sec7").click(function(){
+        $("#sec7 p").html("X");
+        })
+    $("#sec8").click(function(){
+        $("#sec8 p").html("O");
+        })
+    $("#sec9").click(function(){
+        $("#sec9 p").html("X");
+        })
+    
+    // Time to add the logic to the game. 
+
+    
+})
